c1b7c5fbd50cf8b45a90b55fe78aacda97e0ad1c
sakshamagarwal <sakshamagarwal@google.com>
2020-08-15 21:56:15 -0400

    updated config

--- homatransport/src/dcntopo/run-vectio-sender.sh
+++ homatransport/src/dcntopo/run-vectio-sender.sh
@@ -1,1 +1,1 @@
-../homatransport -u Cmdenv -c InFileDist -r 3 -n ..:../../simulations:../../../inet/examples:../../../inet/src -l ../../../inet/src/INET vectioSenderTransportConfig.ini
+../homatransport -u Cmdenv -c Expname -r 1 -n ..:../../simulations:../../../inet/examples:../../../inet/src -l ../../../inet/src/INET vectioSenderTransportConfig.ini
